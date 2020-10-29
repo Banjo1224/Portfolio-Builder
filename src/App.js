3f8593336f810ae2e6c3a87ba15ead6b8d25ddc6
Brandon <bbates377@gmail.com>
2020-10-29 07:48:57 -0500

    fix gitignore for cleaner repo

--- src/App.js
+++ src/App.js
@@ -34,55 +34,70 @@ class App extends React.Component {
     console.log(data);
 
     // const formData = new FormData(); formData.append('file', this.state);
-    axios({
-      method: 'post',
-      url: 'http://localhost:1337/portfolio',
-      data: data})
+    axios.post('http://localhost:1337/portfolio', data)
       .then(res => {
         console.log(res);
-      }).catch(err => console.log(err))
+        return axios.get('http://localhost:1337/portfolio/run-1')
+      })
+      .then(res => {
+        console.log(res)
+        axios.get('http://localhost:1337/portfolio/run-2', { 'responseType': 'blob' })
+          .then(({ data }) => {
+            const downloadUrl = window.URL.createObjectURL(new Blob([data]));
+            const link = document.createElement('a');
+            link.href = downloadUrl;
+            link.setAttribute('download', 'file.zip'); //any other extension
+            document.body.appendChild(link);
+            link.click();
+            link.remove();
+          })
+          .catch((response) => {
+            console.error("Could not Download the Excel report from the backend.", response);
+          });
+      })
+      .catch(err => console.log(err));
   }
 
-  handleProfile(name, email, resume, linkedin, github, description) {
-    resume = resume.slice(0, -resume.split('').reverse().join('').indexOf('/')) + 'export?format=pdf';
-    this.setState({
-      name,
-      email,
-      resume,
-      linkedin,
-      github,
-      description
-    }, () => {
-      console.log(this.state)
-    });
-  }
+handleProfile(name, email, resume, linkedin, github, description) {
+  resume = resume.slice(0, -resume.split('').reverse().join('').indexOf('/')) + 'export?format=pdf';
+  this.setState({
+    name,
+    email,
+    resume,
+    linkedin,
+    github,
+    description
+  }, () => {
+    console.log(this.state)
+  });
+}
 
-  handlePageProps(pageColor, navColor) {
-    this.setState({
-      bg_color: pageColor,
-      nav_color: navColor
-    }, () => {
-      // console.log(this.state.bg_color, this.state.nav_color)
-    })
-  }
+handlePageProps(pageColor, navColor) {
+  this.setState({
+    bg_color: pageColor,
+    nav_color: navColor
+  }, () => {
+    // console.log(this.state.bg_color, this.state.nav_color)
+  })
+}
 
-  render() {
-    return (
-      <div className="App">
-        <header class="App-header">
-          <h1>Portfolio Builder - Software Engineering</h1>
-          <p id='description'>Welcome to the Software Engineering Portfolio Generator. Use this page to create a simple Vue.JS portfolio page ready to deploy!</p>
-        </header>
-          <Profile handleProfile={this.handleProfile.bind(this)}/>
-          <Projects />
-          <Page handleProps={this.handlePageProps.bind(this)} />
-          <button onClick={(e) => {
-            e.preventDefault();
-            this.handleSubmit();
-          }}>Submit Portfolio</button>
-      </div>
-    );
-  }
+render() {
+  return (
+    <div className="App">
+      <header class="App-header">
+        <h1>Portfolio Builder - Software Engineering</h1>
+        <p id='description'>Welcome to the Software Engineering Portfolio Generator. Use this page to create a simple Vue.JS portfolio page ready to deploy!</p>
+      </header>
+      <Profile handleProfile={this.handleProfile.bind(this)} />
+      <Projects />
+      <Page handleProps={this.handlePageProps.bind(this)} />
+      <button onClick={(e) => {
+        e.preventDefault();
+        this.handleSubmit();
+      }}>Submit Portfolio</button>
+    </div>
+  );
+}
 }
 
 export default App;
